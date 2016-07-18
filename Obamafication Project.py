29019fb6be2401af944e24dddadef8f997fa1689
D-Astudillo-ASC <daniela9525@bla2hs.org>
2016-07-18 09:41:41 -0400

    Project

--- Obamafication Project.py
+++ Obamafication Project.py
@@ -3,18 +3,18 @@ Batman = makePicture("Batman.JPG") # Takes JPG image from Calico Folder and save
 show(Batman) # Shows image
 Pixels=getPixels(Batman) # Returns a list of individual pixels from the image.
 # Variables that will be responsible for returning colors to make pixels from.
-ObamaDarkBlue = makeColor(0,51,76)
-ObamaRed = makeColor(217, 26, 33)
-ObamaBlue = makeColor(112,150,158)
-ObamaYellow = makeColor(252, 227, 166)
+Obama_DarkBlue = makeColor(0,51,76)
+Obama_Red = makeColor(217, 26, 33)
+Obama_Blue = makeColor(112,150,158)
+Obama_Yellow = makeColor(252, 227, 166)
 # for loop will repeat the code in its body, or the conditional statements written below. 
 for i in Pixels: # for every item in Pixels
     greyPixels = getGray(i) # This variable gets the gray value of every pixel in the image.
     if greyPixels>180: # if the gray value of a pixel is greaten than 180
-         setColor(i, ObamaYellow) # Changes the color of a pixel to yellow.
+         setColor(i, Obama_Yellow) # Changes the color of a pixel to yellow.
     elif greyPixels>120 and greyPixels<180: # if the gray value is between 120 and 180
-         setColor(i, ObamaBlue) # Changes the color to blue .
+         setColor(i, Obama_Blue) # Changes the color to blue .
     elif greyPixels>60 and greyPixels<120: # if gray value is between 60 and 120 
-         setColor(i, ObamaRed) # Changes the color to red.
+         setColor(i, Obama_Red) # Changes the color to red.
     else: # Otherwise
-         setColor(i, ObamaDarkBlue) # Changes color to blue.
+         setColor(i, Obama_DarkBlue) # Changes color to blue.
